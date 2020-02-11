3ba541ad25a3bbd79ed0f7e387a39486a0ee42a6
Slav <slalev57@me.com>
2020-02-11 07:45:20 -0500

    added index html to link to app js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Write code here:
-ReactDOM.render(<h1>Render me !</h1>, document.getElementById('app')); 
+ReactDOM.render(<h1>Render me !</h1>, document.getElementById('container')); 
