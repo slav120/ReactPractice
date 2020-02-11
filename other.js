963f8b0a91969d6c8d608232f8085fc041176664
Slav <slalev57@me.com>
2020-02-11 08:07:06 -0500

    dom render examples

---
+++ other.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+var myList = (
+<ul>
+<li>hello</li>
+<li>world</li>
+</ul> ); 
+
+ReactDOM.render(
+myList, 
+document.getElementById('app')
+); 
