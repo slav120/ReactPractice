59438e69e878a2cfe4ea2bcb33832c4643f6f0ab
Slav <slalev57@me.com>
2020-02-11 08:10:07 -0500

    cant render the same thing multiple times in one file

---
+++ reactDom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const hello = <h1>Hello world</h1>;
+
+// This will add "Hello world" to the screen:
+
+ReactDOM.render(hello, document.getElementById('app'));
+
+// This won't do anything at all:
+
+ReactDOM.render(hello, document.getElementById('app'));
