92a1071d067169ad959696948eede93d63ea0d54
Slav <slalev57@me.com>
2020-02-12 12:33:40 -0500

    small math example

---
+++ math/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const math = <h1>2 + 3 = {2 + 3}</h1>;
+
+ReactDOM.render(math, document.getElementById('app')
+);
+
+
+
+// Renders a page that displays 
+
+// 2 + 3 = 5 
