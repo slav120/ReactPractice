a814b8f6bf0e1e1912c6f9cf9c154ea676d43b89
Slav <slalev57@me.com>
2020-02-16 18:44:37 -0500

    mapping in jsx

---
+++ mapingJSX.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const people = ['Rowe', 'Prevost', 'Gare'];
+
+const peopleLis = people.map(person =>
+  // expression goes here:
+  <li>{person}</li>
+);
+
+// ReactDOM.render goes here:
+ReactDOM.render(<ul>{peopleLis}</ul>, document.getElementById('app'));
+
+// maping the array to print all of the elements of the array, 
+// displays a webpage with all the items mappend of the array, one after another 
