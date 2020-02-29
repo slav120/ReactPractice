5372a042b148e817dd3fefdb920324ab4fb06e5f
Slav <slalev57@me.com>
2020-02-29 17:32:29 -0500

    passed the greeting prop to app js

--- RenderDifferentUI_BasedOnProps/App.js
+++ RenderDifferentUI_BasedOnProps/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
         <h1>
           Hullo and, "Welcome to The Newzz," "On Line!"
         </h1>
-        <Greeting name="Alison" />
+        <Greeting name="Alison" signedIn={true}/>
         <article>
           Latest:  where is my phone?
         </article>
